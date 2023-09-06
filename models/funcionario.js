f0a2bf148825bce19c7641a8067b96be79dc554d
gabrielbrito01 <143821578+gabrielbrito01@users.noreply.github.com>
2023-09-06 15:31:00 -0300

    Delete models directory

--- models/funcionario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const employeeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  cpf: { type: String, required: true, unique: true }, 
-  age: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Employee', employeeSchema);
