264ceb54860ee605db00c8e1a0af1068a36bfefe
gabrielbrito01 <143821578+gabrielbrito01@users.noreply.github.com>
2023-09-06 14:03:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const authenticate = require('./middleware/authenticate');
+
+require('dotenv').config();
+
+const secretKey = process.env.SECRET_KEY;
 
 mongoose.connect('mongodb://localhost:27017', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.get('/', (req, res) => {
+    res.send('Bem-vindo!');
+  });
+
 const userRoutes = require('./routes/userRoutes');
 app.use('/users', userRoutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-})
 
 const productRoutes = require('./routes/productRoutes');
 app.use('/products', productRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const employeeRoutes = require('./routes/employeeRoutes');
+app.use('/employees', employeeRoutes);
+
+const promotionRoutes = require('./routes/promotionRoutes');
+app.use('/promotion', promotionRoutes);
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
 });
