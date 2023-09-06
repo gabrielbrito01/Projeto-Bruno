e02e94c905442003389ddeb072992e5180ef0278
gabrielbrito01 <143821578+gabrielbrito01@users.noreply.github.com>
2023-09-06 14:46:19 -0300

    Update usuarioroute.js

--- routes/usuarioroute.js
+++ routes/usuarioroute.js
@@ -1,34 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const userController = require('../controller/userController');
 
+router.post('/authenticate', userController.authenticateUser);
 
-router.post('/register', async (req, res) => {
-  try {
-    const { username, password, role } = req.body;
-    const user = new User({ username, password, role });
-    await user.save();
-    res.status(201).json({ message: 'Usuário registrado com sucesso!' });
-  } catch (err) {
-    res.status(500).json({ error: 'Erro ao registrar usuário' });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user || user.password !== password) {
-      return res.status(401).json({ error: 'Credenciais inválidas' });
-    }
-
-    res.json({ message: 'Login bem-sucedido' });
-  } catch (err) {
-    res.status(500).json({ error: 'Erro ao fazer login' });
-  }
-});
-
-router.post('/', userController.createUser);
+router.route('/').post((req, res) => userController.createUser(req, res))
 
 router.get('/', userController.getAllUsers);
 
@@ -37,7 +13,5 @@ router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 
 router.delete('/:id', userController.deleteUser);
-
-router.post('/authenticate', userController.authenticateUser)
 
 module.exports = router;
