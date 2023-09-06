e1965b3b4cc0dba890465a460045e25e88adae31
gabrielbrito01 <143821578+gabrielbrito01@users.noreply.github.com>
2023-09-06 15:05:53 -0300

    Update promoçãoroute.js

--- routes/promoçãoroute.js
+++ routes/promoçãoroute.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const promotionController = require('../controllers/promotionController');
 
 router.get('/get-promotions-by-user/:userId', promotionController.getPromotionsByUser);
+router.post('/', promotionController.createPromotion);
+router.get('/', promotionController.getAllPromotions);
+router.get('/:promotionId', promotionController.getPromotionById);
+router.put('/:promotionId', promotionController.updatePromotion);
+router.delete('/:promotionId', promotionController.deletePromotion);
 
 module.exports = router;
