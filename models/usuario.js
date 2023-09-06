90fa588c987868d4eb31f3a475781420a35570e6
gabrielbrito01 <143821578+gabrielbrito01@users.noreply.github.com>
2023-09-06 15:08:09 -0300

    Update usuario.js

--- models/usuario.js
+++ models/usuario.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['employee', 'customer'], required: true },
+  role: { type: String, enum: ['employee', 'client'], required: true },
+
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('user', userSchema)
+module.exports = User
